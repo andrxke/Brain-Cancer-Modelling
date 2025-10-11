e31f38886c36e8925a2405f09efb92809848ee49
nohyp3 <ake05814@gmail.com>
2025-10-10 23:27:12 -0400

    Add brats2023_updated as a normal folder

---
+++ brats2023_updated/datasets/brats_dataset.py
@@ -0,0 +1,103 @@
+from torch.utils.data import Dataset
+import os
+import nibabel as nib
+from ..processing.preprocess import znorm_rescale, center_crop
+import numpy as np
+import torch
+
+class BratsDataset(Dataset):
+    """Dataset class for loading BraTS training and test data.
+    
+    Args:
+        data_dir: Directory of training or test data.
+        mode: Either 'train', 'test', or 'val' specifying which data is being loaded.
+              'val' mode tries to load segmentation but handles missing files gracefully.
+    """
+    def __init__(self, data_dir, mode):
+        self.data_dir = data_dir
+        self.subject_list = os.listdir(data_dir)
+        self.mode = mode
+
+    def __len__(self):
+        return len(self.subject_list)
+    
+    def load_nifti(self, subject_name, suffix):        
+        """Loads nifti file for given subject and suffix."""
+
+        nifti_filename = f'{subject_name}-{suffix}.nii.gz'
+        nifti_path = os.path.join(self.data_dir, subject_name, nifti_filename)
+        nifti = nib.load(nifti_path)
+        return nifti
+    
+    def load_subject_data(self, subject_name):
+        """Loads images (and segmentation if in train/val mode) and extra info for a subject."""
+
+        modalities_data = []
+        for suffix in ['t1c', 't1n', 't2f', 't2w']:
+            modality_nifti = self.load_nifti(subject_name, suffix)
+            modality_data = modality_nifti.get_fdata()
+            modalities_data.append(modality_data)
+
+        if self.mode == 'train':
+            seg_nifti = self.load_nifti(subject_name, 'seg')
+            seg_data = seg_nifti.get_fdata()
+            return modalities_data, seg_data
+        elif self.mode == 'val':
+            # Try to load segmentation, but handle missing files gracefully
+            try:
+                seg_nifti = self.load_nifti(subject_name, 'seg')
+                seg_data = seg_nifti.get_fdata()
+                return modalities_data, seg_data
+            except FileNotFoundError:
+                # No segmentation file available
+                return modalities_data, None
+        elif self.mode == 'test':
+            return modalities_data
+    
+    def __getitem__(self, idx):
+        subject_name = self.subject_list[idx]
+
+        # Load the data and extra info.
+        if self.mode == 'train':
+            imgs, seg = self.load_subject_data(subject_name)
+        elif self.mode == 'val':
+            result = self.load_subject_data(subject_name)
+            if len(result) == 2:
+                imgs, seg = result
+            else:
+                imgs = result
+                seg = None
+        elif self.mode == 'test':
+            imgs = self.load_subject_data(subject_name)
+
+        # Do Z-score norm and rescaling preprocessing.
+        imgs = [znorm_rescale(img) for img in imgs]
+
+        # Perform center crop.
+        imgs = [center_crop(img) for img in imgs]
+
+        imgs = [x[None, ...] for x in imgs]
+        imgs = [np.ascontiguousarray(x, dtype=np.float32) for x in imgs]
+
+        # Convert to torch tensors.
+        imgs = [torch.from_numpy(x) for x in imgs]
+
+        # If train or val mode, process segmentation similarly (if available).
+        if self.mode == 'train':
+            seg = center_crop(seg)
+            seg = seg[None, ...]
+            seg = np.ascontiguousarray(seg)
+            seg = torch.from_numpy(seg)
+
+            return subject_name, imgs, seg
+        elif self.mode == 'val':
+            if seg is not None:
+                seg = center_crop(seg)
+                seg = seg[None, ...]
+                seg = np.ascontiguousarray(seg)
+                seg = torch.from_numpy(seg)
+                return subject_name, imgs, seg
+            else:
+                return subject_name, imgs, None
+        elif self.mode == 'test':
+            return subject_name, imgs
